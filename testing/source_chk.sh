1149a63d047e66750702ffab4f42bd76c10fe984
RKL <RKL>
2015-04-20 00:40:24 -0700

    Add testing script

--- testing/source_chk.sh
+++ testing/source_chk.sh
@@ -4,7 +4,4 @@ cur_dir=$(pwd -P)
 
 cd ..
 
-find src \( -iname "*.cpp" -o -iname "*.h" -o -iname "*.ipp" \) -exec bash -c "sha512sum '{}' >> $cur_dir/source.sha512" ';'
-find src -maxdepth 1 -iname "Makefile" -exec bash -c "sha512sum '{}' >> $cur_dir/source.sha512" ';'
-find .make \( -iname "src4obj.h" -o -iname "dependency.h" \) -exec bash -c "sha512sum '{}' >> $cur_dir/source.sha512" ';'
-chmod 600 "$cur_dir/source.sha512"
+sha512sum -w -c "$cur_dir/source.sha512" | grep -v "OK"
